c606b479be078a7c20984b2835454dc847d206bd
Josh Stoik <josh@blobfolio.com>
2022-03-08 12:24:47 -0800

    misc: drop redundant test

--- benches/dowser.rs
+++ benches/dowser.rs
@@ -13,10 +13,6 @@ use std::{
 };
 
 benches!(
-	Bench::new("dowser::Dowser", "with_path(/usr/share)")
-		.timed(Duration::from_secs(6))
-		.with(|| Dowser::default().with_path("/usr/share").collect::<Vec<_>>()),
-
 	Bench::new("dowser::Dowser", "from(/usr/share)")
 		.timed(Duration::from_secs(6))
 		.with(|| Dowser::from(PathBuf::from("/usr/share")).collect::<Vec<_>>()),
