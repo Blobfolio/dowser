b371448168d32e0fe7820f2769e72ac7c350274e
Josh Stoik <josh@blobfolio.com>
2022-08-22 13:30:22 -0700

    bump: brunch 0.3

--- benches/dowser.rs
+++ benches/dowser.rs
@@ -10,29 +10,25 @@ use dowser::{
 	Dowser,
 	Extension,
 };
-use std::time::Duration;
 
 
 const GZ: Extension = Extension::new2(*b"gz");
 
 
 benches!(
-	Bench::new("dowser::Dowser", "from(/usr/share)")
-		.timed(Duration::from_secs(6))
-		.with(|| Dowser::from("/usr/share").collect::<Vec<_>>()),
+	Bench::new("dowser::Dowser::from(/usr/share)")
+		.run(|| Dowser::from("/usr/share").collect::<Vec<_>>()),
 
 	Bench::spacer(),
 
-	Bench::new("dowser::Dowser::from(/usr/share)", "filter(gz)")
-		.timed(Duration::from_secs(6))
-		.with_setup(
+	Bench::new("dowser::Dowser::from(/usr/share)::filter(gz)")
+		.run_seeded(
 			Dowser::from("/usr/share"),
 			|d| d.filter(|p| Some(GZ) == Extension::try_from2(p)).collect::<Vec<_>>()
 		),
 
-	Bench::new("dowser::Dowser::from(/usr/share)", "into_vec(gz)")
-		.timed(Duration::from_secs(6))
-		.with_setup(
+	Bench::new("dowser::Dowser::from(/usr/share)::into_vec(gz)")
+		.run_seeded(
 			Dowser::from("/usr/share"),
 			|d| d.into_vec(|p| Some(GZ) == Extension::try_from2(p))
 		),
